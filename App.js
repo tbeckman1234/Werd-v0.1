7b9ce8bdada36b6687e7e18d3d7c61a519723413
Thomas <tbeckman1234@gmail.com>
2023-12-12 14:26:13 -0700

    initial commit

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Keep a list of words and their definitions on-hand. Search and filter by keyword.</Text>
       <StatusBar style="auto" />
     </View>
   );
